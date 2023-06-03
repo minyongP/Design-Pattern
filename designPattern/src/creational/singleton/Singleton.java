989c43f69ee6483320b3ff54ea1a094deeebb69b
minyongP <86906386+Minon98@users.noreply.github.com>
2023-06-03 18:49:19 +0900

    Add singleton

---
+++ designPattern/src/creational/singleton/Singleton.java
@@ -0,0 +1,10 @@
+package creational.singleton;
+
+public class Singleton {
+    private  static  class singleInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+    public static Singleton getInstance() {
+        return singleInstanceHolder.INSTANCE;
+    }
+}
